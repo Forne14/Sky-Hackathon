d918f507d2b6d085880c31d83f96be9905289385
Haroon Qadri <hqadri2k@gmail.com>
2018-01-26 15:27:14 +0000

    final product

--- sky_hackathon1.py
+++ sky_hackathon1.py
@@ -1,38 +1,52 @@
 from tkinter import *
+import urllib.request
+from imdbpie import Imdb
+from bs4 import BeautifulSoup
+import datetime
 
 class window(Tk):
     def __init__(self, main):
         Tk.__init__(self)
-        main.geometry('{}x{}'.format(800, 600))
+        main.geometry('{}x{}'.format(1600, 600))
         self.var_states(main)
         self.movies = ['Movie 1', 'Movie 2', 'Movie 3', 'Movie 4', 'Movie 5']
+        self.genres = ['Action', 'Comedy', 'Horror', 'Thriller', 'Animation', 'Foreign', 'Kids']
         self.labels = []
         self.renderFilms(main)
 
-    master = Tk()
+        self.imdb = Imdb()
+        url = "http://www.xmltv.co.uk/feed/7147"
+        request = urllib.request.Request(url)
+        response = urllib.request.urlopen(request)
+        xml = response.read().decode('utf-8')
+
+        self.beauty = BeautifulSoup(xml)
+        self.data = []
+
+        master = Tk()
 
     def var_states(self, main):
 
         Label(main, text='Choose your favourite movies').grid(row=0, sticky=W)
         self.var = IntVar(main)
 
-        Radiobutton(main, text='action', variable = self.var, value=1).grid(row=1, sticky=W)
+        Radiobutton(main, text='Action', variable = self.var, value=0).grid(row=1, sticky=W)
 
 
-        Radiobutton(main, text='comedy', variable = self.var, value =2).grid(row=2, sticky=W)
+        Radiobutton(main, text='Comedy', variable = self.var, value =1).grid(row=2, sticky=W)
 
 
-        Radiobutton(main, text='horror', variable = self.var, value = 3).grid(row=3, sticky=W)
+        Radiobutton(main, text='Horror', variable = self.var, value =2).grid(row=3, sticky=W)
 
 
-        Radiobutton(main, text='thriller', variable = self.var, value = 4).grid(row=4, sticky=W)
+        Radiobutton(main, text='Thriller', variable = self.var, value = 3).grid(row=4, sticky=W)
 
 
-        Radiobutton(main, text='animation', variable = self.var, value = 5).grid(row=5, sticky=W)
-        Radiobutton(main, text='foreign', variable = self.var, value = 6).grid(row=6, sticky=W)
-        Radiobutton(main, text='kids', variable = self.var, value = 7).grid(row=7, sticky=W)
+        Radiobutton(main, text='Animation', variable = self.var, value = 4).grid(row=5, sticky=W)
+        Radiobutton(main, text='Foreign', variable = self.var, value = 5).grid(row=6, sticky=W)
+        Radiobutton(main, text='Kids', variable = self.var, value = 6).grid(row=7, sticky=W)
 
-        Button(main, text='Submit', command = self.mock_scraper).grid(row=8, sticky=W, pady=4)
+        Button(main, text='Submit', command = lambda:self.searchGenre(self.genres[self.var.get()])).grid(row=8, sticky=W, pady=4)
 
     def renderFilms(self, main):
 
@@ -54,33 +68,36 @@ class window(Tk):
         label5.grid(row=14, sticky = W)
 
         self.labels= [label1, label2, label3, label4, label5]
-
-    def mock_scraper(self):
-        value = self.var.get()
-        if value == 0:
-            self.movies = ['Lord of the Rings', 'The Accountant', 'Taken', 'John Wick']
-        elif value == 1:
-            self.movies = ['Rush Hour', 'Meet Dave', 'Thor Ragnarok', 'The Hangover']
-        elif value == 2:
-            self.movies = ['Saw', 'Insidious', 'Purge', 'Scream']
-        elif value == 3:
-            self.movies = ['Get Out' ]
-        elif value == 4:
-            self.movies = ['Howls Moving Castle']
-        elif value == 5:
-            self.movies = ['The Blue One']
-        elif value == 6:
-            self.movies = ['Lion King']
-        self.setMovies()
-
-
-
-    def setMovies(self):
-        self.labels[0].config(text=self.movies[0])
-        self.labels[1].config(text=self.movies[1])
-        self.labels[2].config(text=self.movies[2])
-        self.labels[3].config(text=self.movies[3])
-        self.labels[4].config(text=self.movies[4])
+        
+    def searchGenre(self, genre):
+        self.data = []
+        for i in range(len(self.labels)):
+            self.labels[i].config(text = self.movies[i])
+        titles = self.beauty.find_all('title')
+        descs = self.beauty.find_all('desc')
+        channels = self.beauty.find_all('channel')
+        print(channels[0].get_text())
+        print(channels[1].get_text())
+        for i in range(len(titles)):
+            if(i == 10):
+                print(self.data)
+                break
+            print(titles[i].get_text())
+            search = self.imdb.search_for_title(titles[i].get_text())
+            if(search != []):
+                idno = search[0].get('imdb_id')
+                try:
+                    genres = self.imdb.get_title_genres(idno).get('genres')
+                    print(genres)
+                except:
+                    pass
+                if genre in genres:
+                    rating = self.imdb.get_title_ratings(idno).get('rating')
+                    self.data.append([titles[i].get_text(), descs[i].get_text(), channels[i].get_text().replace("\n", ""), genre, rating])
+                for j in range(len(self.data)):
+                    if j>=5:
+                        pass
+                    self.labels[j].config(text=self.data[j])
 
 
 
--- xmlscraper.py
+++ xmlscraper.py
@@ -19,12 +19,19 @@ def searchGenre(genre):
     print(channels[0].get_text())
     print(channels[1].get_text())
     for i in range(len(titles)):
+        if(i == 10):
+            print(data)
+            quit()
         print(titles[i].get_text())
         search = imdb.search_for_title(titles[i].get_text())
         if(search != []):
             idno = search[0].get('imdb_id')
-            genre = imdb.get_title_genres(idno)
-            genres = genre.get('genres')
-            for(
-            data.append([titles[i].get_text(), descs[i].get_text(), channels[i].get_text().replace("\n", "")])
-    print(data)
+            try:
+                genres = imdb.get_title_genres(idno).get('genres')
+                print(genres)
+            except:
+                pass
+            if genre in genres:
+                rating = imdb.get_title_ratings(idno).get('rating')
+                data.append([titles[i].get_text(), descs[i].get_text(), channels[i].get_text().replace("\n", ""), genre, rating])
+print(data)
